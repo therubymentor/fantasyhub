8056c3efccd99094769357c40ec780066efb5d04
thatrubylove <thatrubylove@gmail.com>
2014-04-17 10:51:08 -0500

    swapped out rake test for much faster ruby based loader

--- test/ci/ci_runner.sh
+++ test/ci/ci_runner.sh
@@ -1,8 +1,1 @@
-engine=$(ruby -e 'puts RUBY_ENGINE')
-
-case $engine in
-  "ruby" )
-    bundle exec rake test && bundle exec cane;;
-  * )
-    bundle exec rake test;;
-esac
+bundle exec ./run_test_suite
