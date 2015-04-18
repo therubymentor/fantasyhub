adeab00f70f8ae5d6c9733fa037d3ccace84eb50
Jim O'Kelly (rubycasts.io) <thatrubylove@gmail.com>
2015-04-17 19:45:59 -0500

    added a basic integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -3,11 +3,11 @@ require "thincloud/test"
 require 'fantasyhub'
 
 describe "End to end live test against github" do
-  subject { Fantasyhub::Feed::Downloader.download("tenderlove") }
+  subject { Fantasyhub.event_scores_sum_for("tenderlove") }
 
   it "should return the correct actor to verify api connection" do
     VCR.turned_off do
-      JSON.parse(subject)[0]["actor"].must_equal "tenderlove"
+      subject.must_equal 148
     end
   end
 end
