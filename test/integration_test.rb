71e3cd58b1bb25cc1aadd436648085217e6124d6
Jim O'Kelly (rubycasts.io) <thatrubylove@gmail.com>
2015-04-17 19:47:57 -0500

    commented new test for screencast

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -2,12 +2,12 @@ require 'json'
 require "thincloud/test"
 require 'fantasyhub'
 
-describe "End to end live test against github" do
-  subject { Fantasyhub.event_scores_sum_for("tenderlove") }
-
-  it "should return the correct actor to verify api connection" do
-    VCR.turned_off do
-      subject.must_equal 148
-    end
-  end
-end
+# describe "End to end live test against github" do
+#   subject { Fantasyhub.event_scores_sum_for("tenderlove") }
+#
+#   it "should return the correct actor to verify api connection" do
+#     VCR.turned_off do
+#       subject.must_equal 148
+#     end
+#   end
+# end
