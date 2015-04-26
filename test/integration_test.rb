7507c56cae5b5533bbee03d0d721c05582108d57
Jim O'Kelly (rubycasts.io) <thatrubylove@gmail.com>
2015-04-26 14:32:52 -0500

    moving integration test user off a real active user so the score doesnt change!

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -3,11 +3,11 @@ require "thincloud/test"
 require 'fantasyhub'
 
 describe "End to end live test against github" do
-  subject { Fantasyhub.event_scores_sum_for("tenderlove") }
+  subject { Fantasyhub.event_scores_sum_for("fantasyhubber") }
 
   it "should return the correct actor to verify api connection" do
     VCR.turned_off do
-      subject.must_equal 148
+      subject.must_equal 1
     end
   end
 end
