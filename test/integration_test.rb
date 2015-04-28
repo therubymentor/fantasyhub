ca896dc033d5b289081265f2d1e18fbc3095e928
Jim O'Kelly (rubycasts.io) <thatrubylove@gmail.com>
2015-04-27 23:27:52 -0500

    updates for updated github

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,11 +1,10 @@
-require 'json'
 require "thincloud/test"
 require 'fantasyhub'
 
 describe "End to end live test against github" do
   subject { Fantasyhub.event_scores_sum_for("fantasyhubber") }
 
-  it "should return the correct actor to verify api connection" do
+  it "expects the score for fantasyhubber to be 1" do
     VCR.turned_off do
       subject.must_equal 1
     end
